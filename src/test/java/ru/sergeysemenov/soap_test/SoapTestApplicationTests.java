1e9b196f468f9182d9576826d684775431c1dcc0
Abrazacs <sergeysemenov87@gmail.com>
2022-04-17 23:06:42 +0300

    Initial commit

---
+++ src/test/java/ru/sergeysemenov/soap_test/SoapTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sergeysemenov.soap_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
